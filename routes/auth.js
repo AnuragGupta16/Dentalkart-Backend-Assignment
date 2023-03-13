70c65d1191f9d7555dcdaab2072311575824fba0
Anurag Gupta <86673132+AnuragGupta16@users.noreply.github.com>
2023-03-13 20:25:36 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const {db}=require("../config/db");
+
 
 const {login,register} =require("../controllers/usercontroller");
 
